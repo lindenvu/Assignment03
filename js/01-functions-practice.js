052dc71b2894261e36b4a58d82f89ba7d38c80d4
lindenvu <github@lindenvu.com>
2018-02-22 21:50:09 -0800

    Update 1.0

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,9 +1,35 @@
+/*eslint-env browser*/
 //STEP 1
-
+function halfNumber(aNumber) {
+    "use strict";
+    window.console.log("Half of " + aNumber + " is " + aNumber / 2);
+    return aNumber / 2;
+}
+halfNumber(parseInt(window.prompt("Enter a number"), 10));
 //STEP 2
-
+function squareNumber(aNumber) {
+    "use strict";
+    window.console.log("The result of squaring the number " + aNumber + " is " + aNumber * aNumber);
+    return aNumber * aNumber;
+}
+squareNumber(parseInt(window.prompt("Enter a number"), 10));
 //STEP 3
-
+function percentOf(numberOne, numberTwo) {
+    "use strict";
+    var percent = Math.round((numberOne / numberTwo) * 100) + "%";
+    window.console.log(numberOne + " is " + percent + " of " + numberTwo);
+    return percent;
+}
+percentOf(parseInt(window.prompt("Enter two numbers separated by a comma"), 10));
 //STEP 4
-
+function findModulus(numberOne, numberTwo) {
+    "use strict";
+    window.console.log(numberOne % numberTwo + " is the modulus of " + numberOne + " and " + numberTwo);
+    return numberOne % numberTwo;
+}
+findModulus(parseInt(window.prompt("Enter two numbers separated by a comma"), 10));
 //STEP 5
+function sumNumbers() {
+    "use strict";
+}
+sumNumbers(parseInt(window.prompt("Enter several numbers separated by commas"), 10));
