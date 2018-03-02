fc4659753928626e402756a35051645643ad11d1
Linden Vu <lvu@dexcom.com>
2018-03-01 17:17:44 -0800

    Update 4.0

--- js/04-basic-calculator.js
+++ js/04-basic-calculator.js
@@ -7,28 +7,32 @@ var choice = window.prompt("Enter add, subtract, multiply, divide").toLowerCase(
 var runAgain = true;
 
 function calculate(numOne, numTwo, choice) {
-    var answer;
-    if(choice !== "add" && choice !== "subtract" && choice !== "multiply" && choice !== "divide"){
-        window.alert("You entered " + choice + "That is not a valid chocie, let's try again...");
+    "use strict";
+    var answer, msg;
+    if (choice !== "add" && choice !== "subtract" && choice !== "multiply" && choice !== "divide") {
+        window.alert("You entered " + choice + ", that is not a valid chocie, let's try again...");
         runAgain = true;
     } else {
-  
-        switch(choice) {
-            case "add":
-                code block
-                break;
-            case "subtract":
-                code block
-                break;
-            case "multiply":
-                code block
-                break;
-            case "divide":
-                code block
-                break;
+        switch (choice) {
+        case "add":
+            msg = " plus ";
+            answer = numOne + numTwo;
+            break;
+        case "subtract":
+            msg = " minus ";
+            answer = numOne - numTwo;
+            break;
+        case "multiply":
+            msg = " times ";
+            answer = numOne * numTwo;
+            break;
+        case "divide":
+            msg = " divided by ";
+            answer = numOne / numTwo;
+            break;
         }
-        window.alert(numOne + " " + choice + " is " + answer);
-        rungAgain = false;
+        window.alert(numOne + msg + numTwo + " is " + answer);
+        runAgain = false;
     }
 }
 
